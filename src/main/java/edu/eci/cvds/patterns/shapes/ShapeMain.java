a4941f2b4835190571366223d4eb68680e7ce649
carolina <maria.morales@mail.escuelaing.edu.co>
2024-02-07 22:29:33 -0500

    punto final

--- src/main/java/edu/eci/cvds/patterns/shapes/ShapeMain.java
+++ src/main/java/edu/eci/cvds/patterns/shapes/ShapeMain.java
@@ -11,14 +11,14 @@ public class ShapeMain
         }
         try {
             RegularShapeType type = RegularShapeType.valueOf(args[0]);
-            /*Shape shape = ShapeFactory.create(type);
+            Shape shape = ShapeFactory.create(type);
             System.out.println(
                     String.format(
                             "Successfully created a %s with %s sides.",
                             type,
                             shape.getNumberOfEdges()
                     )
-            )*/;
+            );
         } catch (IllegalArgumentException ex) {
             System.err.println(
                     "Parameter '" + args[0] + "' is not a valid RegularShapeType"
